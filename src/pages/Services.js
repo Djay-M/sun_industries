25ba59fdba2dd20ea85402cfd860096336890121
Dhananajaya <dhananjay.m1805@gmailcom>
2024-01-20 19:57:05 +0530

    fix: padding issue in services.cards

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -15,7 +15,7 @@ function Services() {
     <div className="w-full h-screen sm:h-full mt-5">
       <div className="w-full flex flex-col justify-center items-center">
         <span className="text-5xl font-bold text-white">Services</span>
-        <div className="sm:w-[90%] h-[600px] m-5 bg-[#f6f6f6] rounded-3xl">
+        <div className="p-5 sm:w-[90%] h-[600px] m-5 bg-[#f6f6f6] rounded-3xl">
           <Carousel cards={serviceCards()} autoSlide={true} />
         </div>
       </div>
